73f433d9aef04cfed203482b6b61eaef0551d95b
Karthik-Git-Hub1 <pinnojukarthik14@gmail.com>
2026-01-07 10:06:11 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,7 +11,7 @@ def deployTomcat(jobname,ip,context)
 {
    sh '''
 scp webapp/target/webapp.war \
-ubuntu@116.112.67.97:/var/lib/tomcat10/webapps/testapp.war
+ubuntu@${}:/var/lib/tomcat10/webapps/testapp.war
 '''
 
 }
