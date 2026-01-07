44131895c6db48b379573ee84231b219399d2229
Karthik-Git-Hub1 <pinnojukarthik14@gmail.com>
2026-01-07 09:22:12 +0530

    cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,20 +1,17 @@
-def gitDownload(repo) {
-    git "https://github.com/IntelliqDevops/${repo}.git"
+def gitDownload(repo)
+{
+  git "https://github.com/IntelliqDevops/${repo}.git"
+  
 }
-
-def buildArtifact() {
-    sh 'mvn clean package'
+def buildArtifact()
+{
+  sh 'mvn package'
 }
-
-def newDeploy(jobname, ip, context) {
-    sh """
-        scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war \
-        ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war
-    """
+def deployTomcat(jobname,ip,context)
+{
+   sh "scp webapp/target/webapp.war/${jobname}//home/ubuntu/testapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/testapp.war" ${context}.war"
 }
-
-def runSelenium(jobname) {
-    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+def executeSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
-
-
