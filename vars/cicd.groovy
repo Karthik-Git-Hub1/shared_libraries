60849c3cd7755569940a59a9de52e8811db4a3ca
Karthik-Git-Hub1 <pinnojukarthik14@gmail.com>
2026-01-07 10:12:08 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,10 +10,10 @@ def buildArtifact()
 def deployTomcat(jobname,ip,context)
 {
    sh '''
-scp webapp/target/webapp.war \
-ubuntu@16.112.67.97:home/ubuntu/testapp.war
+scp webapp/target/webapp.war ubuntu@16.112.67.97:/home/ubuntu/testapp.war
 ssh ubuntu@16.112.67.97 "sudo mv /home/ubuntu/testapp.war /var/lib/tomcat10/webapps/testapp.war"
-  '''
+'''
+
 
 }
 def executeSelenium(jobname)
